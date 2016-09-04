1f0ec4381e81d61222aa27b843e68d39769b59e8
Mus Bai <mephisto9000@MacBook-Pro-Mus.local>
2016-09-04 12:26:54 +0600

    working on bike racers

--- Hackerrank/BikeRacers/src/Solution.java
+++ Hackerrank/BikeRacers/src/Solution.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
@@ -14,10 +18,33 @@ public class Solution {
 	
 	int[][] bikerPos;
 	int[][] bikePos;
-	double[][] dist;  //bikers // bikes
+	
+	long[][] dist;  //bikers // bikes
+
+	Node[] bikerNodes;
+	Node[] bikeNodes;
+
+	List<Node> nodeList;
+
 	private String fname;
+
+	int V;
+
+	Node s; // source
+	Node sink;
+
+	//here we'll keep the ordered time;
+
+	long timeArray[];
+	long timeAnsArray[];
+	int flowArray[];
+	int parent[];
 	
 	
+	long minTime = Long.MAX_VALUE;
+	
+	
+	/*
 	public class Result{
 		
 		int score;
@@ -28,8 +55,425 @@ public class Solution {
 			this.bikes = bikes;
 		}
 	}
+	*/
 	
-	public Double  calc() throws Exception
+	public class Node{
+		int id;
+		List<Edge> edges;
+		
+		public Node(int id)
+		{
+			this.id = id;
+			edges = new LinkedList<Edge>();
+			
+		}
+		
+		public void setId(int id)
+		{
+			this.id = id;
+		}
+		
+		public void addEdge(Edge e)
+		{
+			edges.add(e);
+		}
+		
+		public void clearEdges()
+		{
+			edges = new LinkedList<Edge>();
+		}
+		
+	}
+	
+	
+	public class Edge
+	{
+		Node from;
+		Node to;
+		int capacity = 0;
+		int flow = 0;
+		
+		boolean isResidual;
+		
+		Edge(Node from, Node to, int capacity, boolean isResidual)
+		{
+			this.from = from;
+			this.to= to;
+			this.capacity = capacity;
+			
+			from.addEdge(this);
+		}
+		
+		public int getRemainCapacity()
+		{
+			return capacity - flow;
+		}
+		
+		public void addFlow(int val)
+		{
+			flow += val;
+		}
+		
+		public void modifyCapacity(int val)
+		{
+			this.capacity += val;
+		}
+	}
+	
+	
+	
+	public boolean dfs(Node src, Node sink,   int[] parent)
+	{
+		if (src == sink)
+			return true;
+		
+		Queue<Node> q = new LinkedList<Node>();
+		
+		q.add(src);
+		
+		boolean visited[] = new boolean[V];
+		
+		Arrays.fill(visited,  false);
+		
+		visited[src.id] = true;
+		
+		while(q.isEmpty() == false)
+		{
+			Node tmp = q.poll();
+			
+			for (int i = 0; i < tmp.edges.size(); i++)
+			{
+				Edge e = tmp.edges.get(i);
+				Node to = e.to;
+				if (e.getRemainCapacity() > 0 && visited[to.id]==false)
+				{
+					visited[to.id] = true;
+					q.add(to);
+					
+					parent[to.id] = tmp.id;
+					
+					if (sink  == to )
+						return true;					
+				}
+			}
+			
+		}
+		
+		return false;
+
+	}
+	
+	
+	/*
+	 *  belman-fordesque implementation of max flow algorithm
+	 */
+	public long calcBikeNum()
+	{
+		int graphHeight = n > m ? n : m;
+		
+		
+		
+		int nodeCounter = 0;
+		
+		//public Node(int id)
+		//Edge(Node from, Node to, int capacity, boolean isResidual)
+		s = new Node(nodeCounter);
+		nodeCounter++;
+		sink = new Node(1000000);
+		
+		nodeList = new ArrayList<Node>();
+		nodeList.add(s);
+		
+		bikerNodes = new Node[n];
+		bikeNodes = new Node[m];
+		
+		for (int i = 0; i < n; i++)
+		{
+			Node b = new Node(nodeCounter);
+			nodeCounter++;
+			nodeList.add(b);
+			
+			
+			Edge e = new Edge(s, b, 1, false);
+			Edge _e = new Edge(b, s, 1, true);
+			
+			bikerNodes[i] = b;			
+		}
+		
+		for (int i = 0; i < m; i++)
+		{
+			Node bb = new Node(nodeCounter);
+			nodeList.add(bb);
+			nodeCounter++;
+			
+			
+			
+			bikeNodes[i] = bb;
+		}
+		
+		sink.setId(nodeCounter);
+		nodeCounter++;
+		nodeList.add(sink);
+		
+		int maxTime = 0;
+		@SuppressWarnings("unchecked")
+		Queue<Long> timeQueue = new PriorityQueue();
+		
+		/*
+		new Comparator<Long>() {
+			@Override
+		    public int compare(Long n1, Long n2) {
+		        // compare n1 and n2
+		    	if (n1 < n2)
+		    		return 1;
+		    	
+		    	if (n1 > n2)
+		    		return -1;
+		    	
+		    	return 0;
+		    }
+		}); */
+		
+		
+		for (int i = 0; i < n; i++)
+			for(int j = 0; j < m; j++)
+				//if (dist[i+1][j+1] > maxTime)
+				{
+					//maxTime = dist[i+1][j+1]
+					if (!timeQueue.contains(dist[i+1][j+1]))
+						timeQueue.add(dist[i+1][j+1]);
+				}
+		
+		
+		
+	
+		
+		parent = new int[nodeCounter];
+		Arrays.fill(parent, -1);
+		
+		V = nodeCounter;
+		long time ;
+		
+		timeArray = new long[timeQueue.size()];
+		timeAnsArray = new long[timeQueue.size()];
+		flowArray = new int[timeQueue.size()];
+		
+		
+		Arrays.fill(timeAnsArray, -1);
+		Arrays.fill(flowArray, 0);
+		
+		for (int i = 0 ; i< timeQueue.size(); i++)
+		{
+			timeArray[i] = timeQueue.poll();
+			
+			//System.out.println(timeArray[i]);
+		}
+		
+		long minTime = Long.MAX_VALUE;
+		
+		int timeIdMin = 0;
+		int timeIdMax = timeArray.length - 1;
+		//int timeId = 0;
+		
+		
+		return bikeSearchBinary(0, timeIdMax );
+ 		 
+	}
+	
+	
+	
+	long bikeSearchBinary(int timeIdMin, int timeIdMax)
+	{
+		// we first clean our network of useless (now) edges
+		 //time = timeQueue.poll(); 
+		 //System.out.println("time == "+time);
+		 
+		int timeId = (timeIdMax + timeIdMin) / 2;
+		
+		//System.out.println("tid == "+timeId+" timeIdMin == "+timeIdMin + " timeIdMax == "+timeIdMax);
+		long time = timeArray[timeId];
+		 
+		 s.clearEdges();
+		
+		for (int i = 0; i < n; i++)
+		{
+			bikerNodes[i].clearEdges();
+			
+			Edge e = new Edge(s, bikerNodes[i], 1, false);
+			Edge _e = new Edge(bikerNodes[i], s, 1, true);
+		}
+		
+		
+		for (int i = 0; i < m; i++)
+		{
+			bikeNodes[i].clearEdges();
+			
+			Edge e = new Edge(bikeNodes[i], sink, 1, false);
+			Edge _e = new Edge(sink, bikeNodes[i], 1 , true);
+		}
+		
+		
+		
+		// here we rebuild our network
+		
+		for (int i = 0; i < n; i++)
+		{
+			for (int j =0; j < m; j++)
+			{
+				if (dist[i+1][j+1] <= time)
+				{
+					Edge e = new Edge(bikerNodes[i], bikeNodes[j], 1, false);
+					Edge e1 = new Edge( bikeNodes[j], bikerNodes[i], 1, true);
+				}
+			}
+		}
+		
+		
+		int flow = 0 ;//Integer.MAX_VALUE;
+		
+		while(dfs(s, sink, parent))
+		{
+			
+			//System.out.println("i'm here");
+			
+			Node t = sink;
+			int minCapacity = Integer.MAX_VALUE;
+			
+			//calculating minimal capacity
+			while(parent[t.id] != -1)
+			{
+				Node t1 = nodeList.get(parent[t.id]);
+				for (int j = 0; j< t1.edges.size(); j++)
+				{
+					Edge e = t1.edges.get(j);
+					if (e.to.id == t.id )
+					{
+						if (minCapacity > e.getRemainCapacity() )
+						{
+							minCapacity = e.getRemainCapacity();
+							break;
+						}
+					}
+						
+					
+				}
+				
+				 
+				t = nodeList.get(parent[t.id]);
+			}
+			
+		   flow += minCapacity;
+			
+			t = sink;
+			while(parent[t.id] != -1)
+			{
+				
+				Node t1 = nodeList.get(parent[t.id]);
+				
+				
+				for (int j = 0; j < t1.edges.size(); j++)
+				{
+					Edge e = t1.edges.get(j);
+					if (e.to.id == t.id)
+					{
+						e.addFlow(minCapacity);
+						Node to = e.to;
+						
+						for (int k = 0; k < to.edges.size(); k++)
+						{
+							Edge _e = to.edges.get(k);
+							
+							if (_e.to.id == t1.id)
+							{
+								_e.addFlow(-minCapacity);
+							}
+							
+						}
+						
+						break;
+ 						
+					}
+					
+				}
+				
+				t = nodeList.get(parent[t.id]);
+			}
+ 			
+		}
+		
+		//System.out.println("flow == " + flow+" k == "+k);
+		
+		flowArray[timeId] = flow;
+		
+		if (flow ==  k)
+		{
+			
+			
+			
+			if (time < minTime)
+				minTime = time;
+			/*
+			
+			timeAnsArray[timeId] = time;
+			
+			long val1 = Long.MAX_VALUE;
+			long val2 = Long.MAX_VALUE;
+			
+			
+			
+			if (timeId - 1 >=0 && timeAnsArray[timeId - 1]==-1)
+				bikeSearchBinary(timeIdMin - 1, timeIdMax -1 );
+			if (timeId + 1 < timeArray.length && timeAnsArray[timeId + 1]==-1)
+				bikeSearchBinary(timeIdMin + 1, timeIdMax + 1 );
+				//< timeArray.length)
+			//	return  bikeSearchBinary(timeIdMin - 1, timeIdMax -1 ); /// (int) time;
+			//else
+				return minTime;
+				*/
+			
+			 //if(time >= K && ( mid == 1 || A[mid-1] < K ))
+			
+			
+			if (timeId - 1 >= 0 ) // timeArray.length)
+			{
+				long val = bikeSearchBinary(timeId - 1, timeId  -1 );
+				
+				if (flowArray[timeId - 1] < k)
+					return time;
+			}
+			//else
+			
+			//return minTime;
+											
+		}
+		
+		if (timeIdMin == timeIdMax)
+			 return time;
+		
+		//if (minTime != Long.MAX_VALUE)
+		//	return minTime;
+		
+		//if (timeId)
+		
+		if (flow >= k)
+		{
+			if (timeId - 1 >= 0)
+				timeId --;
+			return bikeSearchBinary(timeIdMin, timeId );
+		}
+		else
+		{
+			if (timeId + 1 < timeIdMax)
+				timeId ++;
+			return bikeSearchBinary(timeId, timeIdMax);
+		}
+		
+		
+	
+	}
+
+
+	public long  calc() throws Exception
 	{
 		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedReader br;
@@ -48,7 +492,7 @@ public class Solution {
 		bikerPos = new int[n][2];
 		bikePos = new int[m][2];
 		
-		dist = new double[n+1][m+1];
+		dist = new long[n+1][m+1];
 		
 		for (int i = 0; i < n; i++)
 		{
@@ -66,10 +510,7 @@ public class Solution {
 		
 		for (int i = 0; i <= n; i++)
 		{
-			//vals = br.readLine().split(" ");
-			//bikerPos[i][0] = Integer.parseInt(vals[0]);
-			//bikerPos[i][1] = Integer.parseInt(vals[1]);
-			Arrays.fill(dist[i], 0);
+ 			Arrays.fill(dist[i], 0);
 		}
 		
 		
@@ -77,243 +518,21 @@ public class Solution {
 		{
 			for (int j = 0; j < m;j ++)
 			{
-				double d =    ( Math.pow((bikePos[j][0] - bikerPos[i][0]),2) + Math.pow(bikePos[j][1] - bikerPos[i][1], 2));
+				int d =    (int) ( Math.pow((bikePos[j][0] - bikerPos[i][0]),2) + Math.pow(bikePos[j][1] - bikerPos[i][1], 2));
 				
 				dist[i+1][j+1] = d;
-				
-				System.out.println(d);
+   
 			}
 		}
 		
-		/*
-		Set cycles = new HashSet();
-		
-		int ans = (int) minSumDist(0, cycles);
-		System.out.println(ans  );
-		*/
-		return kuhn();
+		 
+		return calcBikeNum();
 		
 		
 		
 	}
 	
-	public double kuhn()
-	{
-		//List<Integer> p = new LinkedList<Integer>();
-		//List<Integer> way = new LinkedList<Integer>();
-		//List<Integer> u = new LinkedList<Integer>();
-		//List<Integer> v = new LinkedList<Integer>();
-		int u[] = new int[n+1];
-		Arrays.fill(u,  0);
-		
-		int v[] = new int[m+1];
-		Arrays.fill(v, 0);
-		
-		
-		int p[] = new int[m+1];
-		Arrays.fill(p, 0);
-		int way[] = new int [m+1];
-		
-		for (int i = 1; i <= n; i++)
-		{
-			p[0] = i;
-			
-			int j0 = 0;
-			
-			double minv[] = new double[m+1];
-			Arrays.fill(minv, Double.MAX_VALUE);
-			
-			boolean used[]= new boolean[m+1];
-			Arrays.fill(used, false);
-			
-			
-			do
-			{
-				used[j0] = true;
-				
-				int i0 = p[j0];
-				double delta = Double.MAX_VALUE;
-				
-				int j1 = 0;//Integer.MAX_VALUE; 
-				
-				for (int j = 1; j <= m; j++)
-				{
-					if (!used[j]) 
-					{
-						double cur = dist[i0][j] - u[i0] -v[j];
-						
-						if (cur < minv[j])
-						{
-							minv[j] = cur;
-							way[j] = j0;
-						}
-						
-						if (minv[j] < delta)
-						{
-							delta = minv[j];
-							j1 = j;
-						}
-						
-						
-						
-					}
-				}
-				
-				
-				for (int j = 0; j <=m; j++)
-				{
-					if (used[j])
-					{
-						u[p[j]] += delta;
-						v[j] -= delta;
-					}
-					else
-						minv[j] -= delta;
-				}
-				
-				j0 = j1;
-			}while(p[j0]!= 0);
-
-			do
-			{
-				int j1 = way[j0];
-				
-				p[j0] = p[j1];
-				j0 = j1;
-				
-			}
-			while (j0>0);
-			
-			
-		}
-		
-		
-		int[] ans = new int [n+1];
-		
-		Arrays.fill(ans, 0);
-		
-		for (int j=1; j<=m; ++j)
-			{
-			ans[p[j]] = j;
-			
-			
-			
-			}
-		
-		double max = 0;
-		
-		Queue q = new PriorityQueue();
-		
-		
-		
-		for (int j=1; j<=m; ++j)
-			//if (dist[j-1][ans[j]-1] > max) // != j)
-				q.add(dist[j][ans[j]]);
-		
-		for (int i = 0;  i < k; i++)
-		{
-			max = (Double) q.poll();
-			System.out.println(max);
-		}
-			//System.out.println(q.poll());
-			
-		//while.
-
-		
-
-		System.out.println((int) max);
-		
-		
-		
-		
-		return max;
-	}
-	
-	public double minSumDist(int cyclerId, Set cycles)
-	{
-		
-		
-		if (cyclerId >=  k ) 						
-				//return new Result(0, cycles.size()); //Float.MAX_VALUE;
-			//return 0;
-			return Float.MAX_VALUE;
-		
-		
-		if (cyclerId == n-1 && cycles.size() < k-1)
-			return Float.MAX_VALUE;
-		
-		//if (cyclerId cycles.size() < k)
-		//	//return new Result(Float.MAX_VALUE-1, cycles.size());;
-			
-			
-		
-		//if (cyclerId >=  n
-		
-		double min = Float.MAX_VALUE;
-		
-		for (int i = 0; i < m; i++)
-		{
-			if (!cycles.contains(i))
-			{
-				
-				//System.out.println(cyclerId+" "+i);
-				double d = dist[cyclerId][i];
-				
-				cycles.add(i);
-				double v = 0;
-				
-				
-				
-				if (cyclerId + 1 < n && cycles.size() < k ) 
-					v = minSumDist(cyclerId+1, cycles);
-				
-				
-				double tmpMin = d > v ? d : v;
-				
-				if (tmpMin == Float.MAX_VALUE)
-					continue;
-				
-				/*
-				System.out.println(cycles.toString());
-				
-				if (cyclerId == 0)
-				{
-				System.out.println("cyclerId == " + cyclerId);
-				System.out.println("tmpMin == "+ tmpMin);
-				System.out.println("d == "+ d);
-				System.out.println("v == "+ v);
-				}
-				*/
-				
-				if (tmpMin < min)
-					min = tmpMin;
-				
-				cycles.remove(i);
-				
-				
-			}
-		}
-
-		
-		double v = Integer.MAX_VALUE;
-
-		if (cyclerId + 1 < n) 
-		v = minSumDist(cyclerId+1, cycles);
-		
-		/*
-		if (cyclerId == 0)
-		{
-			System.out.println("not selected " +v);
-		} */
-		
-		if (v < min && v != Float.MAX_VALUE)
-			min = v;
-		
-		
-		//System.out.println(min);
-		return min;
-		
-	}
+	 
 	
 	public void setFilename(String fname)
 	{
@@ -327,7 +546,7 @@ public class Solution {
 		if (args.length > 0)
 			sol.setFilename(args[0]);
 		
-		System.out.println((int) sol.calc().doubleValue());
+		System.out.println((int) sol.calc());
 
 	}
 
