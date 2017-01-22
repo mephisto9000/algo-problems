dc3c41b77302eb72c6d90437e3dc23db16fd3a4e
Mus Bai <mephisto9000@gmail.com>
2017-01-22 22:39:51 +0600

    DP/Equal solved

--- Hackerrank/Equal/src/Solution.java
+++ Hackerrank/Equal/src/Solution.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class Solution {
 	
+	int n;
+	int[] colleagues; 
 	
 	public int[] calc() throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,66 +15,114 @@ public class Solution {
 		int t = Integer.parseInt(br.readLine());
 		
 		int ans[] = new int[t];
+		
+		Arrays.fill(ans, Integer.MAX_VALUE);	
 		for (int i = 0; i < t; i++)
 		{
-			int n = Integer.parseInt(br.readLine());
+			n = Integer.parseInt(br.readLine());
 			
-			int[] colleagues = new int[n];
+			colleagues = new int[n];
+			
+			 
 			
 			String[] vals = br.readLine().split(" ");
 			
-			int ops = 0;
+			int ops = 0;  
 			
 			int shift = 0;
 			
-			int min = Integer.MAX_VALUE;
+			int max = 0 ; 
+			int min = Integer.MAX_VALUE; 
 			for (int j = 0; j < n; j++)
 			{
 				colleagues[j] = Integer.parseInt(vals[j]);
+				 
+				
+				
+				if (colleagues[j] > max)
+					max = colleagues[j];
 				
 				if (colleagues[j] < min)
 					min = colleagues[j];
-			}
-			
-			
-			Arrays.sort(colleagues);
-			
-			for (int j = n-1; j >0; j--)
-			{
-				while(colleagues[j-1] + shift < colleagues[j] )
-				{
-					int dif =  colleagues[j] - colleagues[j-1] - shift;
-					
-					if (dif >= 5)
-					{
-						shift += 5;
-						ops++;
-					}
-					else					
-					if (dif >= 2)
-					{
-						shift += 2;
-						ops++;
-					}
-					else
-					if (dif >= 1)
-					{
-						shift += 1;
-						ops++;
-					}
-						
-				}
 				
-				colleagues[j-1] += shift;
+			
 			}
 			
-			ans[i] = ops;
+			for (int j = 0; j < n; j++)
+			{
+				colleagues[j] = max - colleagues[j] + min;
+			}
 			
 			
-		}
+			 
+			
+			int add = 0;
+			for (int z = 0; z < 6; z++) //5; z++)
+			{
+				ops = 0;
+				for (int j = 0; j < n; j++)
+				{
+					
+					ops += rec(j, z, max);
+				}
+				 
+			 
+			
+			if (ops < ans[i])
+				ans[i] = ops;
+			
+			
+			}
+			
+			
+		}   // i over t
 		
 		return ans;
 	}
+	
+	int rec(int idx, int hand, int max) 
+	{
+		if (idx >= n)
+			return -1;
+		
+		int shift = colleagues[idx];
+		int val = max + hand;  
+		int ans = 0;
+		
+		while(shift < val)
+		{
+			 
+			int dif = val - shift;
+			
+			if (dif >= 5)
+			{
+				shift += 5;  
+				ans ++;  
+				 
+			}
+			else
+			if (dif >= 2)
+			{
+				shift += 2;  
+				ans ++;  
+				 
+			}
+			else			
+			if (dif >= 1)
+			{
+				shift += 1;  
+				ans ++;  
+			}
+			
+			
+		}
+			
+		
+		return ans;
+		
+		
+	}
+	
 
 	public static void main(String[] args) throws Exception  {
 		
