0d8e33d7af3ddd9a06a09721473071d425760cf4
Mus Bai <mephisto9000@gmail.com>
2017-08-13 10:42:45 -0400

    66 fixed

--- LeetCode/src/problem66/Solution.java
+++ LeetCode/src/problem66/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 	{
 		Solution sol = new Solution();
 		
-		int[] arr= {9,8,7,6,5,4,3,2,1,0};
+		int[] arr= {7,4,5,8,9,8,7,6,5,4,3,2,1,0};
 		
 		int a[] = sol.plusOne(arr);
 		
@@ -17,27 +17,44 @@ public class Solution {
         
         long v = 0;
         
+        StringBuffer sb = new StringBuffer();
+        
+        
+        boolean increase = true;
         if (digits.length > 0)
         {
             long z = 1;
             for (int i = digits.length -1; i >=0; i--)
             {
-                long x = digits[i] * z;
+                int x = digits[i];
                 
-                v += x;
-                z *= 10;
+                if (increase)
+                {
+                	if (x < 9)
+                	{
+                		x++;
+                		increase = false;
+                	}
+                	else
+                		x = 0;
+                }
+                
+                sb.insert(0, Integer.toString(x).charAt(0)); 
                 
             }
+            
+            if (increase)
+            	sb.insert(0, '1');
         }
         
-        v++;
+       
         
-        String s = String.valueOf(v);
+        //String s = String.valueOf(v);
         
-        int ans[] = new int[s.length()];
+        int ans[] = new int[sb.toString().length()];
         
         for (int i = 0; i < ans.length; i++)
-            ans[i] = s.charAt(i) - '0';
+            ans[i] = sb.toString().charAt(i) - '0';
         
         return ans;
         
