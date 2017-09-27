5474e19a56dd616c2e522ea10eeebfc6df3c274f
Mus Bai <mephisto9000@gmail.com>
2017-09-26 22:23:10 -0400

    LC 45 remade

--- LeetCode/src/problem45/Solution.java
+++ LeetCode/src/problem45/Solution.java
@@ -3,49 +3,36 @@ package problem45;
 import java.util.Arrays;
 
 class Solution {
-    int n;
-    int[] nums;
-    int memo[];
-    public int jump(int[] nums) {
-        
-        if (nums == null || nums.length == 0)
-            return 0;
-        
-        n = nums.length;
-        this.nums = nums;
-        memo = new int[n];
-        Arrays.fill(memo, -1);
-        
-        int ans = 0;
-        for(int i = n-1; i>=0; i--)
-        	ans =  rec(i);
-        if (ans == 100000 )
-            return 0 ;
-        return ans;
-    }
-    
-    int rec(int idx)
-    {
-        if (idx >= n-1)
-            return 0; //100000;
-        
-        if (memo[idx]!= -1)
-        	return memo[idx];
-        
-        int min = 1000000;
-        for (int i = 1; i <= nums[idx]; i++)
-        {
-        	
-            int v = 1 + rec(idx + i);
-            if (v < min)
-                min = v;
-            if (i + idx > n)
-            	break;
-        }
-        
-        memo[idx] = min;
-        return min;
-    }
-    
+	  public int jump(int[] nums) {
+			
+	        if (nums == null || nums.length == 0)
+	            return 0;
+	        
+	        int n = nums.length;
+	        
+	        int steps = 0;
+	        int maxDist = nums[0];
+	        int currentDist = 0;
+	        for (int i = 1; i < n; i++)
+	        {
+	        	
+	        	
+	        	if (currentDist < i)
+	        	{
+	        		steps ++;
+	        		currentDist = maxDist; // = i;
+	        	}
+	        	
+	        	maxDist = (maxDist > i + nums[i] ? maxDist : i + nums[i]);
+	        	
+	        	//if (maxDist >= n-1)
+	        	//	return steps;
+	        	
+	        }
+	        return steps;
+	        
+		
+		}
+	}
     
 }
