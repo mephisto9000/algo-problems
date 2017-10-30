c638024262a2ce28118f729f6817de3c2790b771
Mus Bai <mephisto9000@gmail.com>
2017-10-29 20:04:30 -0400

    LC 389

---
+++ LeetCode/src/problem389/Solution.java
@@ -0,0 +1,18 @@
+package problem389;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for (int i = 0; i < c1.length; i++)
+            if (c1[i]!=c2[i])
+                return c2[i];
+        
+        return c2[c1.length];
+        
+    }
+}
