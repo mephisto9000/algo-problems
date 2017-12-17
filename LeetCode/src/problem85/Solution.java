e571ef499389ce89c30eb9841d191b8096702511
Mus Bai <mephisto9000@gmail.com>
2017-12-17 18:49:44 -0500

    LC 85

--- LeetCode/src/problem85/Solution.java
+++ LeetCode/src/problem85/Solution.java
@@ -1,115 +1,65 @@
 package problem85;
 
+import java.util.Stack;
+
 class Solution {
 
-    int mtxHor[][];
-    int mtxVer[][];
+    int mtx[][];
     int w;
     int h;
     public int maximalRectangle(char[][] matrix) {
         
         if (matrix == null || matrix.length == 0)
             return 0;
-        
-        //matrix;
+                
         h = matrix.length;
         w = matrix[0].length;
         
-        mtxHor = new int[h][w];
-        mtxVer = new int[h][w];
-        
+        mtx = new int[h][w];
         
         for (int i = 0; i < h; i++)
-        {
-        mtxHor[i][0] = Character.getNumericValue(matrix[i][0]);
-        mtxVer[i][0] = Character.getNumericValue(matrix[i][0]);
-        }
+            for (int j = 0; j < w; j++)
+                mtx[i][j] = matrix[i][j] -'0';
+              
         
         for (int j = 0; j < w; j++)
-        {
-        mtxHor[0][j] = Character.getNumericValue(matrix[0][j]);
-        mtxVer[0][j] = Character.getNumericValue(matrix[0][j]);
-        }
-        
-        
-        
-        if (h == 1)
-        {
-        	int max = 0;
-        	int lmax = 0;        			
-        	for (int j = 0; j < w; j++)
-        	{
-        		if (matrix[0][j] =='1')
-        			++lmax;
-        		else
-        			lmax = 0;
-        	
-        	if (lmax > max)
-        		max = lmax;
-        	}
-        	
-        	return max;
-        }
-        else
-        if (w == 1)
+        {        
+            for (int i = h-1; i >=0; i--)
             {
-            	int max = 0;
-            	int lmax = 0;        			
-            	for (int i = 0; i < h; i++)
-            	{
-            		if (matrix[i][0] == '1')
-            			++lmax;
-            		else
-            			lmax = 0;
-            	
-            	if (lmax > max)
-            		max = lmax;
-            	}
-            	return max;
-            }
-        
-        
-        for (int i = 1; i < h; i++)
-        {
-            for (int j = 1; j < w; j++)
-                if (matrix[i][j] == '0')
+                if (mtx[i][j]==0 || i == 0)
                 {
-                    mtxHor[i][j] = 0;
-                    mtxVer[i][j] = 0;
+                    int i1 = i+1;                    
+                    while (i1 < h && mtx[i1][j] != 0)
+                    {
+                        mtx[i1][j] = mtx[i1-1][j]+1;
+                        
+                        i1++;                        
+                    }
+                }
+            }
+        } 
+ 
+        Stack<Integer> st = new Stack();
+        int max = 0;
+        for (int i = 0; i < h; i++)
+        {
+            st.clear();
+            for (int j = 0; j <= w; j++)
+            {
+                if (st.isEmpty() || (j < w && mtx[i][j] >= mtx[i][st.peek()]))
+                {
+                    st.push(j);
                 }
                 else
                 {
-                	
-                	/*
-                	if (mtxHor[i][j-1] == 0 || mtxVer[i-1][j] == 0  ) //|| mtxVer[i-1][j-1] == 0  
-                	{
-                		mtxHor[i][j] = 1;
-                		mtxVer[i][j] = 1;                		
-                	}
-                	else */
-                	{
-                		mtxHor[i][j] = mtxHor[i][j-1] + 1;
-                		mtxVer[i][j] = mtxVer[i-1][j] + 1;
-                	}
+                    int top = st.pop();
+                    max = Math.max(max, mtx[i][top] * (!st.isEmpty() ? j - st.peek() - 1 : j));
+                    j--;
                 }
-                
-        }
-        
-        int max = 0;
-        
-        for (int i = 0; i < h; i++)
-        {
-            for (int j = 0; j < w; j++)
-            {
-                int v = mtxHor[i][j] * mtxVer[i][j];
-                System.out.print(v);
-                System.out.print(" ");
-                if (v > max)
-                    max = v;
+                    
             }
-            System.out.println();
         }
-        
+            
         return max;
         
      
