4bfc025c7950dde90d61a3c08c778c0e9d01571a
Mus Bai <mephisto9000@gmail.com>
2018-03-15 23:47:09 -0400

    LC 238

---
+++ LeetCode/src/problem238/Solution.java
@@ -0,0 +1,23 @@
+package problem238;
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+      
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans,1);
+        for (int i = 1; i < n; i++)
+        {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int tmp =1;
+        for (int i = n-2; i>=0; i--)
+        {
+            tmp *= nums[i+1];
+            ans[i] *= tmp; 
+        }
+        
+         
+        return ans;
+    }
+}
