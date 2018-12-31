788edeb533904127990ed9b8c07f415bcd5b37b7
Mus Bai <mephisto9000@gmail.com>
2018-12-30 20:16:27 -0500

    LC 706

---
+++ LeetCode/src/problem706/Solution.java
@@ -0,0 +1,34 @@
+package problem706;
+
+class MyHashMap {
+
+    int vals[];
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        vals = new int[1000001];
+        Arrays.fill(vals,-1);
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        vals[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return vals[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        vals[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
