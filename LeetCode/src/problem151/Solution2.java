69d7b75d56d30e32105344abcca576ff05339bc5
Mus Bai <mephisto9000@gmail.com>
2019-07-17 21:07:00 -0400

    LC 151x2

---
+++ LeetCode/src/problem151/Solution2.java
@@ -0,0 +1,34 @@
+package problem151;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+      
+      int min = nums[0];
+      int max = nums[0];
+      int ans = nums[0];
+      
+      
+      
+      for (int i = 1; i<nums.length; i++) {
+        
+        if (nums[i] > 0) {
+          max = Math.max(max * nums[i], nums[i]) ;
+          min = Math.min(min * nums[i], nums[i]);
+        } else
+        if (nums[i] == 0) {
+          max = 0;
+          min =0;
+        } else // neg          
+        {
+          int tmp = min;
+          min = Math.min(max*nums[i], nums[i]);
+          max = Math.max(tmp * nums[i], nums[i]);
+        }
+        
+        ans = Math.max(ans, max); 
+      }
+            
+      return ans;
+        
+    }
+}
