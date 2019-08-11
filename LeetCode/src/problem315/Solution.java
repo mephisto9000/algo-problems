aebe8b63a17775c589adbf50464adcabcce4ffe6
Mus Bai <mephisto9000@gmail.com>
2019-08-10 20:30:39 -0400

    LC 315 didn't save it seems ^.^

--- LeetCode/src/problem315/Solution.java
+++ LeetCode/src/problem315/Solution.java
@@ -1,5 +1,23 @@
 package problem315;
 
-public class Solution {
-
-}
+class Solution {
+    
+    public List<Integer> countSmaller(int[] nums) {    
+        List<Integer> vals = new ArrayList<>();
+        List<Integer> ans = new LinkedList();        
+        for (int i = nums.length - 1; i>=0; i--) {
+            int v = nums[i];
+            
+            int idx = Collections.binarySearch(vals, v);
+            if (idx < 0) {
+                idx = -idx-1;                                
+            }
+            while(idx-1 >= 0 && idx < vals.size() && vals.get(idx-1)==nums[i]) {
+                idx --;            
+            }
+            vals.add(idx, v);
+            ans.add(idx);                        
+        }        
+        Collections.reverse(ans);
+        return ans;
+    }         
